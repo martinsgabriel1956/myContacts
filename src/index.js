8cb34f9ba0121baf9aa1c3f24b0c33168ff79f9a
Gabriel Martins <martinsgabriel@gmail.com>
2023-02-28 21:33:41 -0300

    feat: Setting Eslint and editorconfig

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
+app.get('/', (request, response) => {
+  response.send('Hello World!');
 });
 
-const serverStartedMessage = () => console.log("Server started on port: 3000");
+const serverStartedMessage = () => console.log('Server started on port: 3000');
 
 app.listen(3000, serverStartedMessage);
