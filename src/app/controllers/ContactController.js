dd5ba3842e536a5933d1f98bce382d2f9190c629
Gabriel Martins <martinsgabriel@gmail.com>
2023-03-02 23:08:51 -0300

    feat: Creating edit contacts route

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -31,7 +31,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
-      return response.status(400).json({ error: 'This e-mail is already been taken' });
+      return response.status(400).json({ error: 'This e-mail is already been use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -41,7 +41,34 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {}
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExistsByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactExistsByEmail && contactExistsByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already been use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
+  }
 
   async delete(request, response) {
     const { id } = request.params;
