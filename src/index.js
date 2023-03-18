15a98ccfab8e2a487661b121826eef4f3955638c
Gabriel Martins <martinsgabriel@gmail.com>
2023-03-18 19:54:43 -0300

    feat: Capturing and handling errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('express-async-errors');
 const express = require('express');
 const router = require('./routes');
 
@@ -5,6 +6,10 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 const serverStartedMessage = () => console.log('Server started on port: 3000');
 
