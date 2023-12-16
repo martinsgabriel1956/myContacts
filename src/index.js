811fef0cbe90f4f85030afd8ecd71d4ec9f3ab4e
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-12-16 19:37:17 -0300

    feat: creating the CORS middleware in the api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-require("express-async-errors");
-const express = require("express");
-const router = require("./routes");
+require('express-async-errors');
+const express = require('express');
+const router = require('./routes');
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(router);
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
-const serverStartedMessage = () => console.log("Server started on port: 3001");
+const serverStartedMessage = () => console.log('Server started on port: 3001');
 
 app.listen(3001, serverStartedMessage);
